9b26d2df751d0a9aba30d4435fadd99a4829a84e
wanderingduc <dkdanid@outlook.com>
2024-10-20 17:33:23 +0200

    Add image gallery with flexbox

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,10 +6,11 @@ import Images from './Images';
 
 function App() {
   const colors = ['red', 'green', 'blue', 'red', 'green', 'blue', 'red', 'green', 'blue', 'red', 'green', 'blue', 'red', 'green', 'blue'];
+  const photos = [{'orient': 'l', 'path': '20241016_0001.png'}, {'orient': 'l', 'path': '20241016_0030.png'}, {'orient': 'p', 'path': '20241016_0018.png'}, {'orient': 'l', 'path': '20241016_0020.png'}, {'orient': 'l', 'path': '20241016_0022.png'}, {'orient': 'l', 'path': '20241016_0024.png'}, {'orient': 'p', 'path': '20241016_0019.png'}, {'orient': 'l', 'path': '20241016_0031.png'}];
   return (
     <>
     <Nav />
-    <Images colormap={colors}/>
+    <Images photomap={photos}/>
     </>
   );
 }
--- portfolio/src/Images.js
+++ portfolio/src/Images.js
@@ -2,16 +2,24 @@ import { usestate } from 'react';
 import './Images.css';
 
 function Images(props){
-  const colormap = props.colormap;
-  const colors = colormap.map((color) => 
-      <div style={{backgroundColor: `${color}`, width: 'min(300px, 30vh)', height: 'min(240px, 24vh)', margin: '25px'}}>{color}</div>
+//  const colormap = props.colormap;
+//  const colors = colormap.map((color) => 
+//      <div style={{backgroundColor: `${color}`, width: 'min(300px, 30vh)', height: 'min(240px, 24vh)', margin: '25px'}}>{color}</div>
+//  );
+
+  const photomap = props.photomap;
+  const photos = photomap.map((photo) => 
+    photo["orient"]==="l" ? <img style={{width: 'min(300px, 30vh)', height: 'min(240px, 24vh)', margin: '25px'}} src={`./assets/photos/${photo["path"]}`} /> : <img style={{width: 'min(240px, 24vh)', height: 'min(300px, 30vh)', margin: '25px'}} src={`./assets/photos/${photo["path"]}`} />
+//    <img style={{width: 'min(240px, 24vh)', height: 'min(300px, 30vh)', margin: '25px'}} src={`./assets/photos/${photo["path"]}`} />
+
   );
+
 
   return (
     <div className='image-container'>
-    {colors}
+    {photos}
     </div>
   )
 }
 
-  export default Images;
+export default Images;
