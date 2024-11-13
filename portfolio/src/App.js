7bf9933a51145a15adee07e6e4a48a92ac019bf0
wanderingduc <dkdanid@outlook.com>
2024-11-13 18:16:41 +0100

    Make name panel

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Nav from './Nav';
 import Headpanel from './Headpanel';
 import Images from './Images';
+import Start from './Start';
 
 function App() {
   const colors = ['red', 'green', 'blue', 'red', 'green', 'blue', 'red', 'green', 'blue', 'red', 'green', 'blue', 'red', 'green', 'blue'];
   const photos = [{'orient': 'l', 'path': '20241016_0001.png'}, {'orient': 'l', 'path': '20241016_0030.png'}, {'orient': 'p', 'path': '20241016_0018.png'}, {'orient': 'l', 'path': '20241016_0020.png'}, {'orient': 'l', 'path': '20241016_0022.png'}, {'orient': 'l', 'path': '20241016_0024.png'}, {'orient': 'p', 'path': '20241016_0019.png'}, {'orient': 'l', 'path': '20241016_0031.png'}];
   return (
     <>
+    <Start />
     <Nav />
     <Images photomap={photos}/>
     </>
---
+++ portfolio/src/Start.js
@@ -0,0 +1,17 @@
+import './Start.css';
+
+function Start() {
+
+
+
+  return (
+    <div className='start-container' style={{backgroundImage: 'url("./assets/photos/20241016_0030.png")', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+      <h1 className='name'>David</h1> 
+    </div>
+  )
+
+
+}
+
+
+export default Start;
